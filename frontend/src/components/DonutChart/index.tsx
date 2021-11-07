7bba909b63dddc35f6da989bdf069bfd8947fcbd
Angelica <angelica.scarvalho@live.com>
2021-11-06 22:27:44 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,23 +1,47 @@
+import axios from 'axios';
+import { BASE_URL } from 'pages/ultis/request';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+
+}
+
 function DonutChart() {
 
-    const mockData = {
+    //forma errada
+    let chartData: ChartData = { labels: [], series: [] };
+    
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            chartData = { labels: myLabels, series: mySeries };
+            console.log(chartData)
+        });
+
+    
+    /*const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }
-    
+    }*/
+
     const options = {
         legend: {
             show: true
         }
     }
     return (
-       <Chart 
-            options={{ ...options, labels: mockData.labels}}
-            series={mockData.series}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
             type="donut"
             height="240"
-       />
+        />
     );
 }
 
---
+++ frontend/src/types/sale.ts
@@ -0,0 +1,4 @@
+export type SaleSum = {
+    sellerName: "Logan",
+    sum: number
+}
