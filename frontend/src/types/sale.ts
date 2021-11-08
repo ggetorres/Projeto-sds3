34e75b4b839c523e4a331b26c6e5ec95da457abe
Angelica <angelica.scarvalho@live.com>
2021-11-07 21:13:44 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,7 +1,31 @@
-export type SaleSum = {
-    sellerName: string;
-    sum: number;
+import { Seller } from "./seller"
+
+export type Sale = {
+    visited: 83,
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+    
 }
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+}
+
+
+export type SaleSum = {
+        sellerName: string;
+        sum: number;
+    }
 
 export type SaleSucess = {
     sellerName: string;
